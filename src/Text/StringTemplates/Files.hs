48ca041f8a752b4b8b3e0e8bdb63697f7000c686
Mariusz Rak <mariusz@scrive.com>
2014-05-14 09:56:47 +0200

    Version that supports templates that clean whiteSpaces (created with =* instead of =)

--- src/Text/StringTemplates/Files.hs
+++ src/Text/StringTemplates/Files.hs
@@ -1,16 +1,18 @@
 -- |module for reading many template definitions from files
 module Text.StringTemplates.Files (getTemplates) where
 
-import Data.Char (isAlphaNum)
+import Data.Char (isAlphaNum,isControl,isSpace)
 import Data.List (intercalate, isPrefixOf)
 import Data.Maybe (maybeToList)
 import System.IO
 
 -- |parses template definitions read from the file path (using utf8)
 -- definitions are separated by >=1 lines starting with '#'
--- if first line of definition looks like foo=bar, then it returns
+-- if first line of definition looks like foo=bar or foo=*bar, then it returns
 -- (foo, bar ++ rest of the lines (from definition) concatenated)
 -- broken definitons are skipped
+--
+-- Templates created with =* will be cleaned from spaces and eol chars.
 --
 -- Example template file:
 --
@@ -48,7 +50,7 @@ parseTemplates handle = do
 
 -- reads many lines from the handle, stopping at
 -- first line starting with '#', (read from the handle, not used)
--- if line should looks like "foo=bar", then it returns
+-- if line should looks like "foo=bar" or "foo=*bar", then it returns
 -- Just (foo, bar ++ rest of the lines concatenated)
 -- otherwise Nothing
 parseTemplate :: Handle -> IO (Maybe (String, String))
@@ -58,7 +60,9 @@ parseTemplate handle = do
     if (null ls || null (name) || null t)
         then return Nothing
         else do
-            let template = intercalate "\r\n" ((tail t): (tail ls))
+            let template = if ("=*" `isPrefixOf` t)
+                            then filter (\c -> not (isControl c)  && not (isSpace c)) $ concat ((drop 2 t): (tail ls))
+                            else intercalate "\r\n" ((tail t): (tail ls))
             return $ Just (filter isAlphaNum name,template)
 
 -- returns list of lines read from the handle
