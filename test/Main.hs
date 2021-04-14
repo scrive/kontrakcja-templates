09720cb48c3a302b647828db509c220d14abcc0e
iori tsu <matsuhara.iori@scrive.com>
2021-04-14 13:11:00 +0200

    add test fixtures

--- test/Main.hs
+++ test/Main.hs
@@ -11,23 +11,25 @@ import Data.List (isPrefixOf)
 
 import Text.StringTemplates.TemplatesLoader (readGlobalTemplates, GlobalTemplates)
 
+
+
 main :: IO ()
 main = do
   defaultMain $
     testGroup "Templates Loader" $
       [ testCase "fetch value from text templates" $ do
-          globalTemplates <- readGlobalTemplates "texts" "templates" "fr"
+          globalTemplates <- readGlobalTemplates textFixtures templateFixtures "fr"
           let mRendered = Map.lookup "fr" globalTemplates >>= getStringTemplate "_Hello" <&> render
           assertEqual "assert same values" (Just "Bonjour .") mRendered
           assertEqual "available languages" ["fr", "jp"] $ Map.keys globalTemplates
 
       , testCase "fetch value from templates" $ do
-          globalTemplates <- readGlobalTemplates "texts" "templates" "fr"
+          globalTemplates <- readGlobalTemplates textFixtures templateFixtures "fr"
           let mRendered = Map.lookup "fr" globalTemplates >>= getStringTemplate "foo" <&> render
           assertEqual "assert same values" (Just "hey") mRendered
 
       , testCase "missing default language should raise an exception" $ do
-          eException <- Exception.try $ readGlobalTemplates "texts" "templates" "cz"
+          eException <- Exception.try $ readGlobalTemplates textFixtures templateFixtures "cz"
           case eException of
             Left (SomeException e) -> do
               assertBool "exception raised" $ "Default language cz is not defined." `isPrefixOf` (show e)
@@ -35,8 +37,14 @@ main = do
               assertFailure "missing language didn't raise an exception"
 
       , testCase "fill template with default language values if missing values" $ do
-          globalTemplates <- readGlobalTemplates "texts" "templates" "jp"
+          globalTemplates <- readGlobalTemplates textFixtures templateFixtures "jp"
           let mRendered = Map.lookup "fr" globalTemplates >>= getStringTemplate "_Hello" <&> render
           assertEqual "assert same values" (Just "Bonjour .") mRendered
           assertEqual "available languages" ["fr", "jp"] $ Map.keys globalTemplates
       ]
+    where
+      templateFixtures :: String
+      templateFixtures = "test/fixtures/templates"
+
+      textFixtures :: String
+      textFixtures = "test/fixtures/texts"
