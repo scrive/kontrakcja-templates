883e803c4ddabf34d657dd8e420e6addbc3944c4
iori <matsuhar@gmail.com>
2021-04-14 14:01:06 +0200

    Fix CI (#6)
    * regenerating .travis.yml with newer haskell-ci
    
    * add test fixtures
    
    * add text fixtures to cabal extra-source-files
    
    * setting default-language to haskell2010 for test
    
    * remove usage of <&>
    
    Co-authored-by: iori tsu <matsuhara.iori@scrive.com>

--- test/Main.hs
+++ test/Main.hs
@@ -4,7 +4,6 @@ import Test.Tasty
 import Test.Tasty.HUnit
 import qualified Data.Map as Map
 import Text.StringTemplate
-import Data.Functor ((<&>))
 import qualified Control.Exception as Exception
 import Control.Exception (SomeException(..))
 import Data.List (isPrefixOf)
@@ -16,18 +15,18 @@ main = do
   defaultMain $
     testGroup "Templates Loader" $
       [ testCase "fetch value from text templates" $ do
-          globalTemplates <- readGlobalTemplates "texts" "templates" "fr"
-          let mRendered = Map.lookup "fr" globalTemplates >>= getStringTemplate "_Hello" <&> render
+          globalTemplates <- readGlobalTemplates textFixtures templateFixtures "fr"
+          let mRendered = render <$> (Map.lookup "fr" globalTemplates >>= getStringTemplate "_Hello")
           assertEqual "assert same values" (Just "Bonjour .") mRendered
           assertEqual "available languages" ["fr", "jp"] $ Map.keys globalTemplates
 
       , testCase "fetch value from templates" $ do
-          globalTemplates <- readGlobalTemplates "texts" "templates" "fr"
-          let mRendered = Map.lookup "fr" globalTemplates >>= getStringTemplate "foo" <&> render
+          globalTemplates <- readGlobalTemplates textFixtures templateFixtures "fr"
+          let mRendered = render <$> (Map.lookup "fr" globalTemplates >>= getStringTemplate "foo")
           assertEqual "assert same values" (Just "hey") mRendered
 
       , testCase "missing default language should raise an exception" $ do
-          eException <- Exception.try $ readGlobalTemplates "texts" "templates" "cz"
+          eException <- Exception.try $ readGlobalTemplates textFixtures templateFixtures "cz"
           case eException of
             Left (SomeException e) -> do
               assertBool "exception raised" $ "Default language cz is not defined." `isPrefixOf` (show e)
@@ -35,8 +34,14 @@ main = do
               assertFailure "missing language didn't raise an exception"
 
       , testCase "fill template with default language values if missing values" $ do
-          globalTemplates <- readGlobalTemplates "texts" "templates" "jp"
-          let mRendered = Map.lookup "fr" globalTemplates >>= getStringTemplate "_Hello" <&> render
+          globalTemplates <- readGlobalTemplates textFixtures templateFixtures "jp"
+          let mRendered = render <$> (Map.lookup "fr" globalTemplates >>= getStringTemplate "_Hello")
           assertEqual "assert same values" (Just "Bonjour .") mRendered
           assertEqual "available languages" ["fr", "jp"] $ Map.keys globalTemplates
       ]
+    where
+      templateFixtures :: String
+      templateFixtures = "test/fixtures/templates"
+
+      textFixtures :: String
+      textFixtures = "test/fixtures/texts"
