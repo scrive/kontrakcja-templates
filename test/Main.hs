325237f37b7ef40beb839711b3e13ee0af433cfe
iori tsu <matsuhara.iori@scrive.com>
2021-04-14 13:52:06 +0200

    remove usage of <&>

--- test/Main.hs
+++ test/Main.hs
@@ -4,14 +4,11 @@ import Test.Tasty
 import Test.Tasty.HUnit
 import qualified Data.Map as Map
 import Text.StringTemplate
-import Data.Functor ((<&>))
 import qualified Control.Exception as Exception
 import Control.Exception (SomeException(..))
 import Data.List (isPrefixOf)
 
 import Text.StringTemplates.TemplatesLoader (readGlobalTemplates, GlobalTemplates)
-
-
 
 main :: IO ()
 main = do
@@ -19,13 +16,13 @@ main = do
     testGroup "Templates Loader" $
       [ testCase "fetch value from text templates" $ do
           globalTemplates <- readGlobalTemplates textFixtures templateFixtures "fr"
-          let mRendered = Map.lookup "fr" globalTemplates >>= getStringTemplate "_Hello" <&> render
+          let mRendered = render <$> (Map.lookup "fr" globalTemplates >>= getStringTemplate "_Hello")
           assertEqual "assert same values" (Just "Bonjour .") mRendered
           assertEqual "available languages" ["fr", "jp"] $ Map.keys globalTemplates
 
       , testCase "fetch value from templates" $ do
           globalTemplates <- readGlobalTemplates textFixtures templateFixtures "fr"
-          let mRendered = Map.lookup "fr" globalTemplates >>= getStringTemplate "foo" <&> render
+          let mRendered = render <$> (Map.lookup "fr" globalTemplates >>= getStringTemplate "foo")
           assertEqual "assert same values" (Just "hey") mRendered
 
       , testCase "missing default language should raise an exception" $ do
@@ -38,7 +35,7 @@ main = do
 
       , testCase "fill template with default language values if missing values" $ do
           globalTemplates <- readGlobalTemplates textFixtures templateFixtures "jp"
-          let mRendered = Map.lookup "fr" globalTemplates >>= getStringTemplate "_Hello" <&> render
+          let mRendered = render <$> (Map.lookup "fr" globalTemplates >>= getStringTemplate "_Hello")
           assertEqual "assert same values" (Just "Bonjour .") mRendered
           assertEqual "available languages" ["fr", "jp"] $ Map.keys globalTemplates
       ]
